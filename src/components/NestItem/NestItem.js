b9845d8edda38f36f52ee23a492e7c287962c18f
Franz Cordes-Eklund <franzcordeseklund@gmail.com>
2020-11-20 11:18:51 -0600

    all styling in one css file atm for simplicity

--- src/components/NestItem/NestItem.js
+++ src/components/NestItem/NestItem.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 import swal from 'sweetalert';
-import './NestItem.css';
+
 
 class NestItem extends Component {
 
